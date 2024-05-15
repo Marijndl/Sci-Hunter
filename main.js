76482dcd8537961874933af263cedfd30089a3bf
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-05-15 18:52:48 +0200

    Get scholar citations

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ chrome.contextMenus.removeAll(function() {
     }); })
 
 chrome.contextMenus.onClicked.addListener(searchSciHub);
+
+APAGenerator = function(title){
+  var query = title.selectionText;
+  query = query.replace(/ /g, "+");
+  chrome.tabs.create({url: "https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=" + query + "&btnG=#d=gs_cit&t=1715790737208&u=%2Fscholar%3Fq%3Dinfo%3AL8-BvBf-jVAJ%3Ascholar.google.com%2F%26output%3Dcite%26scirp%3D0%26hl%3Den"});
+};
+
+chrome.contextMenus.removeAll(function() {
+chrome.contextMenus.create({
+  id: "2",
+  title: "Get citation from scholar",
+  contexts:["selection"]
+  }); })
+
+chrome.contextMenus.onClicked.addListener(APAGenerator);
