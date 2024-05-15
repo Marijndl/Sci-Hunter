c7877a5c3a6da76e1cd15b50b7c4440f969447d3
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-05-15 19:52:17 +0200

    Add APA API

--- main.js
+++ main.js
@@ -3,27 +3,64 @@ searchSciHub = function(DOI){
     chrome.tabs.create({url: "https://www.sci-hub.se/" + query});
   };
 
-  
-chrome.contextMenus.removeAll(function() {
-  chrome.contextMenus.create({
-    id: "1",
-    title: "Search in Sci Hub",
-    contexts:["selection"]
-    }); })
-
-chrome.contextMenus.onClicked.addListener(searchSciHub);
-
 APAGenerator = function(title){
   var query = title.selectionText;
   query = query.replace(/ /g, "+");
   chrome.tabs.create({url: "https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=" + query + "&btnG=#d=gs_cit&t=1715790737208&u=%2Fscholar%3Fq%3Dinfo%3AL8-BvBf-jVAJ%3Ascholar.google.com%2F%26output%3Dcite%26scirp%3D0%26hl%3Den"});
 };
 
-chrome.contextMenus.removeAll(function() {
-chrome.contextMenus.create({
-  id: "2",
-  title: "Get citation from scholar",
-  contexts:["selection"]
-  }); })
+getAPA = function(DOI){
+  var query = DOI.selectionText;
+  var apiUrl = "https://api.citeas.org/product/" + query + "?email=test@example.com";
 
-chrome.contextMenus.onClicked.addListener(APAGenerator);
+  fetch(apiUrl)
+  .then(response => {
+    if (response.ok) {
+      return response.json(); // Parse the response data as JSON
+    } else {
+      throw new Error('API request failed');
+    }
+  })
+  .then(data => {
+    // Process the response data here
+    const APA = data.citations[0].citation;
+    console.log(APA);
+    // navigator.clipboard.writeText(APA)
+  })
+  .catch(error => {
+    // Handle any errors here
+    console.error(error); // Example: Logging the error to the console
+  });
+
+};
+
+chrome.contextMenus.removeAll(function() {
+  chrome.contextMenus.create({
+    id: "1",
+    title: "Search in Sci Hub",
+    contexts:["selection"]
+    });
+
+  chrome.contextMenus.create({
+    id: "2",
+    title: "Get citation from scholar",
+    contexts:["selection"]
+    });
+
+  chrome.contextMenus.create({
+    id: "3",
+    title: "Copy APA citation to clipboards",
+    contexts:["selection"]
+    });
+  
+  })
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "1") {
+      searchSciHub(info);
+  } else if (info.menuItemId === "2") {
+      APAGenerator(info);
+  } else if (info.menuItemId === "3") {
+      getAPA(info);
+  }
+});
