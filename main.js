83d5b7db63126f8d55a826cd894a43330c6e7178
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-05-15 17:18:50 +0200

    Upgrade to manifest V3

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ searchSciHub = function(DOI){
     var query = DOI.selectionText;
     chrome.tabs.create({url: "https://www.sci-hub.se/" + query});
   };
+
   
+chrome.contextMenus.removeAll(function() {
   chrome.contextMenus.create({
+    id: "1",
     title: "Search in Sci Hub",
-    contexts:["selection"],
-    onclick: searchSciHub
-  });
+    contexts:["selection"]
+    }); })
+
+chrome.contextMenus.onClicked.addListener(searchSciHub);
